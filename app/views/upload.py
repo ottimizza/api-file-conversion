aab3b2218c0085fd6eea540c9637f113392fb479
Lucas Martins <dev.lucasmartins@gmail.com>
2020-06-10 13:17:30 -0300

    adjustments

--- app/views/upload.py
+++ app/views/upload.py
@@ -1,8 +1,7 @@
 import os
-from flask import abort, Blueprint, g, jsonify, render_template, request, Response, send_file
+from flask import abort, Blueprint, g, jsonify, render_template, request, Response, send_file, app
 from flask_cors import cross_origin
 from werkzeug.utils import secure_filename
-
 
 from app.exceptions import IllegalArgumentsException
 
@@ -49,7 +48,7 @@ def upload_files():
     # files iteration and validation
     # for f in files:
     filename = secure_filename(f.filename)
-    filepath = os.path.join(UPLOAD_FOLDER, filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
     print("saving files...")
 
@@ -76,7 +75,7 @@ def upload_files():
         .parse() \
         .write()
 
-    return send_file(UPLOAD_FOLDER, attachment_filename='out.csv')
+    return send_file(app.config['UPLOAD_FOLDER'], attachment_filename='out.csv')
 
 # if uploaded_file and allowed_file(uploaded_file.filename):
 #     filename = secure_filename(uploaded_file.filename)
