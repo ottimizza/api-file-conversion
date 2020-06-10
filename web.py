4859fab345d04f1f6c9ceddb5b8919340bbff501
Lucas Martins <dev.lucasmartins@gmail.com>
2020-06-10 13:42:36 -0300

    adjustments

--- web.py
+++ web.py
@@ -1,7 +1,25 @@
-from app import create_app
+# from app import create_app
 import os
 
 port = int(os.environ.get("PORT", 5000))
+
+from flask import Flask
+
+def create_app(environment="prod"):
+    # 
+    application = Flask(__name__)
+    application.config.from_object('settings')
+
+    # cors...
+
+    # db...
+
+    # Upload Blueprint
+    from app.views.upload import upload_blueprint
+    application.register_blueprint(upload_blueprint)
+
+    return application
+
 
 if __name__ == "__main__":
     app = create_app()
